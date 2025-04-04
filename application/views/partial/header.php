f5d2d33766e33b5a54ec4385e32454cc863996dd
josem109 <josem109@gmail.com>
2025-04-04 11:57:17 -0400

    Update software branding across multiple language files and views
    - Changed the software short name from "OSPOS" to "NEXTIENDA" in various language files including Azerbaijani, Bosnian, English, Spanish, French, and more.
    - Updated the login view and header to reflect the new branding, ensuring consistency across the application.
    - Adjusted SVG logo dimensions in the login view for better display.

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -128,7 +128,7 @@
 						<span class="icon-bar"></span>
 					</button>
 
-					<a class="navbar-brand hidden-sm" href="<?php echo site_url(); ?>">OSPOS</a>
+					<a class="navbar-brand hidden-sm" href="<?php echo site_url(); ?>">NEXTIENDA</a>
 				</div>
 
 				<div class="navbar-collapse collapse">
